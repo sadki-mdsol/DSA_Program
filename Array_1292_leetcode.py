df584c80aedb50f1c60192dc5142e0d864ca96e4
sadki <sadki@mdsol.com>
2024-02-24 18:09:42 +0530

    1292_leetcode

---
+++ Array_1292_leetcode.py
@@ -0,0 +1,19 @@
+# 1929. Concatenation of Array
+# Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+# Specifically, ans is the concatenation of two nums arrays.
+
+
+
+
+def concatArray(nums):
+    ans = nums.copy()
+
+    for n in nums:
+        ans.append(n)
+    return ans
+
+if __name__ == '__main__':
+    nums = [1,3,5,6]
+    target = 7
+    print(concatArray(nums))
