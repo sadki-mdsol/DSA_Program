1288e253f561ba40f3a6b22426c284ea84212e51
sadki <sadki@mdsol.com>
2024-02-24 18:00:02 +0530

    added another solution

--- Array_35_leetcode.py
+++ Array_35_leetcode.py
@@ -5,6 +5,8 @@
 # You must write an algorithm with O(log n) runtime complexity.
 
 
+# Solution 1 : Nomral way
+
 def searchInsert(nums,target):
     for key,value in enumerate(nums):
         if value == target:
@@ -15,9 +17,32 @@ def searchInsert(nums,target):
             return key+1
         elif value < target and nums[key+1]>target:
             return key+1
+
+
+# Solution 2 : Bianry search
+def binarySearch(nums,target):
+    print(nums,target)
     
+    # l = 0
+    # r = len(nums)-1
+    # N = len(nums)-1
+    le,ri = 0,len(nums)-1
+
+    while le <= ri:
+        mid = (le+ri)//2
+        val = nums[mid]
+
+        if target < nums[mid]:
+            ri = mid-1
+        elif target > nums[mid]:
+            le= mid+1
+        else :
+            return mid
+    return le
+
 
 if __name__ == '__main__':
-    nums = [1]
-    target = 0
-    print(searchInsert(nums,target))
+    nums = [1,3,5,6]
+    target = 7
+    # print(searchInsert(nums,target))
+    print(binarySearch(nums,target))
