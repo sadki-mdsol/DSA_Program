3f97f30c1cde7c1a0fa514890983780bf55e2493
sadki <sadki@mdsol.com>
2024-03-01 20:49:47 +0530

    Python conceptd

---
+++ Python_Basic/Basic_data_types.py
@@ -0,0 +1,28 @@
+
+print( 2+3) #5
+print (9-8) #1
+print(10-10) #0
+print (10-200) #-190
+print(4*6) #24
+print(10/2) #5.0
+print(11//2) #5 will omit the / 
+print(9.0-1) # 8.0
+# print(1-10-)
+#   File "<stdin>", line 1
+#     1-10-
+#          ^
+# SyntaxError: invalid syntax
+print((10-2)*2) #16
+print(10%3) #1
+print(2 ** 3) #powe of 8
+
+#string type
+print('Sneha') #Sneha without single quotes
+print ("Sneha 's Laptop") #1st approach
+print ('Sneha "Laptop"') #Sneha "Laptop"
+print ('Sneha\'s "Laptop"') #Sneha's "Laptop"
+
+print('Sneha'*2) #SnehaSneha
+
+
+
---
+++ Python_Basic/Variables.py
@@ -0,0 +1,45 @@
+# '_' in the ide/python3 sheell it will consider the o/p of the previous executed line
+x= 2
+print(x)
+y=3
+print(y)
+print(x+y)
+
+x=10
+print(x)
+
+str = 'youtube'
+print(str)
+print(str[0])
+print(str[2:6])
+print(str[2:50]) #this will not throw error rather it will traverse till end
+print(str[1:])
+print(str[:5])
+print(str[:])
+print(str[-3])
+print(len(str))
+
+# str[3]='r' #perticualr index of the string can be changes 
+# print(str)
+
+# but we can re-assign all together a new vaue to  the str
+
+str = 'new Value'
+print(str)
+
+
+# sadki@C02F2KFUML7H DSA_Program % python3 Python_Basic/Variables.py
+# 2
+# 3
+# 5
+# 10
+# youtube
+# y
+# utub
+# utube
+# outube
+# youtu
+# youtube
+# u
+# 7
+# new Value
