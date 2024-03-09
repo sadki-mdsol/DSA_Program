8ccf75938b8c71f225c64ebdc6ca1dc33d6d4a43
sadki <sadki@mdsol.com>
2024-03-09 18:16:05 +0530

    anonymous funciton

---
+++ Python_Basic/Anonymous_function.py
@@ -0,0 +1,22 @@
+
+# normal Fucntion 
+def square (n):
+    return n*n
+
+print(square(10))
+
+
+#by using lambdaas/Anonymous function
+
+
+print("by using lambdaas/Anonymous function------------")
+sq = lambda a : a*a
+
+
+print(sq(10))
+
+
+eval = lambda a,b,c : a*b+c
+
+
+print(eval(10,10,20))
