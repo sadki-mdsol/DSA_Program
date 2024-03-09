7b0611191be72a8146244237245f23d8241d75b4
sadki <sadki@mdsol.com>
2024-03-09 19:13:52 +0530

    Module in python

---
+++ Python_Basic/Module.py
@@ -0,0 +1,9 @@
+#import calc 
+#calc.add(10,7)
+
+from Calc import *
+
+print("Add is form another module {}".format(add (10,7)))
+print("Multiplication is {}".format(mul (10,7)))
+print("Division is {}".format(div(10,7)))
+print("Substraction is {}".format(sub(10,7)))
